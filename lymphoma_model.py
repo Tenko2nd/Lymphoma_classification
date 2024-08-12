2617061a73a36052952ad60c4c3a70b07d0bc9cc
Mathis Cassert <mathis.cassert@reseau.eseo.fr>
2024-08-12 11:27:36 +0200

    update lymphoma model training

--- lymphoma_model.py
+++ lymphoma_model.py
@@ -1,4 +1,4 @@
-from lymphoma_dataset import LymphomaDS_resize360
+from lymphoma_dataset import LymphomaDS_resize360_small
 from torch.utils.data import DataLoader, random_split
 from sklearn import preprocessing
 import pickle
@@ -6,15 +6,17 @@ from torchvision import models
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
 # Parameters
 BATCH_SIZE = 32
+WORKERS = 3
 RATIO_TRAIN_VAL_TEST = [0.8, 0.2, 0]
-DATASET = LymphomaDS_resize360
+DATASET = LymphomaDS_resize360_small
 LE_PATH = "Lymphoma_labelEncoder.pkl"
 LEARNING_RATE = 0.001
-SAVE_MODEL_PATH = "Lymphoma_classification_model.pth"
+SAVE_MODEL_PATH = "mod_res50_50ep_3k2.pth"
 
 train_data, test_data = random_split(
     DATASET,
@@ -32,11 +34,15 @@ with open(LE_PATH, "rb") as f:
 
 
 loaders = {
-    "train": DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0),
-    "val": DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0),
+    "train": DataLoader(
+        train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
+    ),
+    "val": DataLoader(
+        test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
+    ),
 }
 
-model = models.resnet18(pretrained=True)
+model = models.resnet50(pretrained=True)
 
 # Freeze all layers except the final classification layer
 for name, param in model.named_parameters():
@@ -54,53 +60,78 @@ model = model.to(device)
 
 from custom_funcions import printProgressBar
 
-# Training loop
-NUM_EPOCHS = 10
-for i in range(NUM_EPOCHS):
-    for phase in ["train", "val"]:
-        if phase == "train":
-            model.train()
-        else:
-            model.eval()
+if __name__ == "__main__":
+    # Training loop
+    NUM_EPOCHS = 50
+    trainLossList, valLossList, trainAccList, valAccList = [], [], [], []
+    for i in range(NUM_EPOCHS):
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()
+            else:
+                model.eval()
 
-        running_loss = 0.0
-        running_corrects = 0
+            running_loss = 0.0
+            running_corrects = 0
 
-        for idx, (inputs, labels) in enumerate(loaders[phase]):
-            inputs = inputs.float().to(device)
-            labels = torch.tensor(le.transform(labels)).to(device)
+            for idx, (inputs, labels, _) in enumerate(loaders[phase]):
+                inputs = inputs.float().to(device)
+                labels = torch.tensor(le.transform(labels)).to(device)
 
-            optimizer.zero_grad()
+                optimizer.zero_grad()
 
-            with torch.set_grad_enabled(phase == "train"):
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels.long())
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels.long())
 
-                if phase == "train":
-                    loss.backward()
-                    optimizer.step()
-            printProgressBar(
-                idx + 1,
-                int(lenDataSet[phase] / BATCH_SIZE) + 1,
-                f"{phase} Progress",
-                "Complete",
-                length=50,
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+                printProgressBar(
+                    idx + 1,
+                    int(lenDataSet[phase] / BATCH_SIZE) + 1,
+                    f"{phase, i+1} Progress",
+                    "Complete",
+                    length=50,
+                )
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / (
+                len(train_data) if phase == "train" else len(test_data)
+            )
+            epoch_acc = running_corrects.double() / (
+                len(train_data) if phase == "train" else len(test_data)
             )
 
-            running_loss += loss.item() * inputs.size(0)
-            running_corrects += torch.sum(preds == labels.data)
+            if phase == "train":
+                trainAccList.append(epoch_acc)
+                trainLossList.append(epoch_loss)
+            else:
+                if epoch_loss < min(valLossList) if valLossList else float("inf"):
+                    print(
+                        "\033[92m "
+                        + "Best model "
+                        + "\033[95m\033[3m"
+                        + f"(acc: {epoch_acc}, loss: {epoch_loss})\033[0m"
+                    )
+                    # Save the model
+                    torch.save(model.state_dict(), SAVE_MODEL_PATH)
+                valAccList.append(epoch_acc)
+                valLossList.append(epoch_loss)
 
-        epoch_loss = running_loss / (
-            len(train_data) if phase == "train" else len(test_data)
-        )
-        epoch_acc = running_corrects.double() / (
-            len(train_data) if phase == "train" else len(test_data)
-        )
+            print(f"{phase, i+1} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
 
-        print(f"{phase, i+1} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+    print("Training complete!")
 
-print("Training complete!")
-
-# Save the model
-torch.save(model.state_dict(), "Lymphoma_classification_model.pth")
+    plt.subplot(2, 1, 1)
+    plt.plot(trainLossList, "g", valLossList, "r")
+    plt.legend(("train", "val"))
+    plt.title("Loss")
+    plt.subplot(2, 1, 2)
+    plt.plot(trainAccList, "g", valAccList, "r")
+    plt.title("Accuracy")
+    plt.legend(("train", "val"))
+    plt.show()

c9efa1db9e02af90e36faf9102bf1ef921d837c6
Mathis Cassert <mathis.cassert@reseau.eseo.fr>
2024-08-28 12:02:05 +0200

    change dataset for k-fold CV

--- lymphoma_dataset.py
+++ lymphoma_dataset.py
@@ -10,9 +10,10 @@ import pandas as pd
 
 
 def df_train_val_test(full_pd_df, st, fract_sample, extern_val):
-    """Create a csv file at the location /Dataset/{name}.csv of the code.
+    """ Create a csv file with a new column folder for later repartition. It as multiple train and no validation for k-fold CV.
         Input a pandas dataframe for it to be worked on. you can choose to have a smaller dataset, using fract_sample.
-        And to have an external validation (one patient can only be in one folder train/val/test) or internal
+        And to have an external validation (one patient can only be in one folder train/val/test) or internal.
+        The file is saved at the location /Dataset/{name}.csv of the code.
 
     Args:
         full_pd_df (pandas.core.frame.DataFrame): The dataset you want to categorize train/val/test
@@ -27,12 +28,15 @@ def df_train_val_test(full_pd_df, st, fract_sample, extern_val):
         raise Exception(
             "Problem with the value of fract_sample, it must be between 0 and 1"
         )
-    # if internal validation 80% train / 20% val
+    # if internal validation 100% train-val
     if not extern_val:
         full_pd_df = full_pd_df.sample(frac=fract_sample)
-        full_pd_df["folder"] = np.where(full_pd_df.index % 5 < 4, "train", "val")
+        full_pd_df["folder"] = np.where(full_pd_df.index % 5 == 0, "train_1",
+                                np.where(full_pd_df.index % 5 == 1, "train_2",
+                                np.where(full_pd_df.index % 5 == 2, "train_3",
+                                np.where(full_pd_df.index % 5 == 3, "train_4", "train_5"))))
 
-    # if external validation 60% train / 20% val / 20% test
+    # if external validation 80% train-val / 20% test
     else:
         # choose random patient (mix them in the process)
         sample_pat = np.random.choice(
@@ -40,17 +44,9 @@ def df_train_val_test(full_pd_df, st, fract_sample, extern_val):
             int(full_pd_df["patient"].nunique() * fract_sample),
             replace=False,
         )
-        train_size = int(len(sample_pat) * 0.6)
-        test_size = int(len(sample_pat) * 0.2)
-        train_patients, val_patients, test_patients = (
-            sample_pat[:train_size],
-            sample_pat[train_size:-test_size],
-            sample_pat[-test_size:],
-        )
-        # Mapping patient in folders train/val/test
-        patient_folder_map = {p: "train" for p in train_patients}
-        patient_folder_map.update({p: "val" for p in val_patients})
-        patient_folder_map.update({p: "test" for p in test_patients})
+        split_pat = np.array_split(sample_pat, 5)
+        # Mapping patient in 5 folders train_(1,2,3,4)/test
+        patient_folder_map = {p: f"train_{i+1}" if i < len(split_pat) - 1 else "test" for i, folder in enumerate(split_pat) for p in folder}
         full_pd_df["folder"] = full_pd_df["patient"].map(patient_folder_map)
 
     # drop data with no folder attributed and save in csv file

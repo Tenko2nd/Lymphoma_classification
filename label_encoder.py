55263b8774d00b651f080a4d50a355c35e8d4bbe
Mathis Cassert <mathis.cassert@reseau.eseo.fr>
2024-11-22 14:48:04 +0100

    doc label encoder over

--- label_encoder.py
+++ label_encoder.py
@@ -1,12 +1,13 @@
 """\033[38;5;224m    This python code is used to create a <Label Encoder> based on the categories of the dataset created, thanks to the code "lymphoma_dataset.py".
     It take the different classes defined in the <classes> intern variable of the dataset and fit them to the label encoder
     \033[38;5;208m//!\\\\ The classes must be references in a variable named 'classes' inside of the __init__ function of the dataset.\033[38;5;224m
-    After that the different classes are serialized in a <Pickle> file with the name "Lymphoma_labelEncoder.pkl".
-    \033[38;5;110m//!\\\\ To deserialize the label encoder and use it you must follow this steps :
+    After, the different classes are serialized in a <Pickle> file with the name "Lymphoma_labelEncoder.pkl".
+    \033[38;5;110m//!\\\\ Here is an example for loading the <Label Encoder> :
         from sklearn import preprocessing                   
         with open('Lymphoma_labelEncoder.pkl', 'rb') as f:  
             le = preprocessing.LabelEncoder()               
-            le.classes_ = pickle.load(f)\033[38;5;213m
+            le.classes_ = pickle.load(f)
+        print(le.classes_)\033[38;5;213m
 """
 
 import argparse

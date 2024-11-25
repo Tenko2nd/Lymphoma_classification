dfec07b0b17c2e7a906b7fd445cd9db221ec9909
Mathis Cassert <mathis.cassert@reseau.eseo.fr>
2024-11-25 13:50:56 +0100

    doc lymphoma dataset over

--- lymphoma_dataset.py
+++ lymphoma_dataset.py
@@ -1,66 +1,57 @@
-"""\033[38;5;224m    This python code is used to create datasets of lymphoma pictures with the data stored in the csv thanks to the code "lymphoma_csv_data.py".
+"""\033[38;5;224m    This python code is used to create CSV datasets of lymphoma pictures with the data stored in the csv thanks to the code "lymphoma_csv_data.py".
     it automatically create a csv with a name based on the inputs given\033[38;5;213m
 """
 
-import argparse
+from argparse import ArgumentParser, RawTextHelpFormatter
 import os
 
 import numpy as np
-import pandas as pd
+from pandas import read_csv, DataFrame
 
 
-def df_train_val_test(full_pd_df, st, no_mult,fract_sample, extern_val):
-    """ Create a csv file with a new column folder for later repartition. It as multiple train and no validation for k-fold CV.
-        Input a pandas dataframe for it to be worked on. you can choose to have a smaller dataset, using fract_sample.
-        And to have an external validation (one patient can only be in one folder train/val/test) or internal.
+def map_images_2_fold(full_pd_df : DataFrame, extern_val : bool = True, k : int = 5, two_classes : bool = False) -> DataFrame:
+    """ Create a csv file with a new column fold for later repartition.
+        Input a pandas dataframe for it to be worked on (The one created in "lymphoma_csv_data.py").
+        You can choose to have an external validation (one patient can only be in one fold train/val/test) or internal.
         The file is saved at the location /Dataset/{name}.csv of the code.
 
     Args:
-        full_pd_df (pandas.core.frame.DataFrame): The dataset you want to categorize train/val/test
-        st (str): the name of the subtype for saving
-        fract_sample (float, optional): the fraction of sample you want from your bigger dataset (between 0 and 1) . Defaults to 1.0.
+        full_pd_df (DataFrame): The dataset you want to categorize train/val/test
         extern_val (bool, optional): if you want external validation or not. Defaults to True.
+        k (int, optional): The number of fold you want in your dataset. Defaults to 5.
+        two_classes (bool, optional): if you don't want the control patients. Defaults to False.
 
-    Raises:
-        Exception: if fract_sample is not between 0 and 1
+    Returns:
+        DataFrame: The dataframe where all patient is assigned to a unique fold
     """
-    if fract_sample > 1 or fract_sample < 0:
-        raise Exception(
-            "Problem with the value of fract_sample, it must be between 0 and 1"
-        )
-    # if internal validation 100% train-val
+
+    if two_classes:
+        full_pd_df = full_pd_df[full_pd_df["categorie"] != "SGTEM"]
+
+    # if internal validation
     if not extern_val:
-        full_pd_df = full_pd_df.sample(frac=fract_sample)
-        full_pd_df["folder"] = np.where(full_pd_df.index % 5 == 0, "folder_1",
-                                np.where(full_pd_df.index % 5 == 1, "folder_2",
-                                np.where(full_pd_df.index % 5 == 2, "folder_3",
-                                np.where(full_pd_df.index % 5 == 3, "folder_4", "folder_5"))))
+        # Create the fold column using vectorized operations
+        full_pd_df["fold"] = "fold_" + ((full_pd_df.index % k) + 1).astype(str)
 
-    # if external validation 80% train-val / 20% test
     else:
-        # choose random patient (mix them in the process)
-        sample_pat = np.random.choice(
-            full_pd_df["patient"].unique(),
-            int(full_pd_df["patient"].nunique() * fract_sample),
-            replace=False,
-        )
-        split_pat = np.array_split(sample_pat, 5)
-        # Mapping patient in 5 folders folder_(1,2,3,4,5)
-        patient_folder_map = {p: f"folder_{i+1}" for i, folder in enumerate(split_pat) for p in folder}
-        full_pd_df["folder"] = full_pd_df["patient"].map(patient_folder_map)
+        # Get unique patients and shuffle them
+        unique_patients = full_pd_df["patient"].unique()
+        np.random.shuffle(unique_patients)
 
-    # drop data with no folder attributed and save in csv file
-    full_pd_df = full_pd_df.dropna(subset=["folder"])
-    full_pd_df.to_csv(
-        f"{os.getcwd()}/Dataset/DS_Lymph_{st if st is not None else ""}{f"{int(fract_sample*100)}p_" if fract_sample!=1 else ""}{"ext" if extern_val else "int"}{"_NM" if no_mult else ""}.csv",
-        index=False,
-        header=True,
-    )
+        # Create a mapping from patient into k fold
+        patient_to_fold = {}
+        for i, patient in enumerate(unique_patients):
+            patient_to_fold[patient] = f"fold_{(i % k) + 1}"
+
+        # Assign the fold to each row in the DataFrame
+        full_pd_df["fold"] = full_pd_df["patient"].map(patient_to_fold)
+
+    return full_pd_df
 
 
-def parser_init():
-    parser = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawTextHelpFormatter
+def parser_init() -> ArgumentParser:
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=RawTextHelpFormatter
     )
 
     parser.add_argument(
@@ -72,60 +63,74 @@ def parser_init():
     )
 
     parser.add_argument(
-        "-st",
-        "--sub_type",
-        help="If you want your dataset to be only about one or multiple cell type (eg. ['LY'] or ['MO', 'SNE'])",
-        default=None,
-        nargs='+',
-        required=False,
-    )
-
-    parser.add_argument(
-        "-fs",
-        "--frac_sample",
-        help="If your dataset is to big, the fraction of data you would like (default : 1)",
-        default=1,
-        type=float,
-        required=False,
-    )
-
-    parser.add_argument(
         "--int",
         dest="extern_val",
         action='store_false',
-        help="If you want your patient to be in multiple categories train/val/test"
+        help="If you want internal validation (one patient in train, val and test all at once)"
     )
-    
+
     parser.add_argument(
-        "--no_mult",
-        dest="no_multiple",
+        "-k",
+        "--number_fold",
+        default=5,
+        type=int,
+        help="The number of fold you want for later k_fold. Defaults to 5.",
+        required=False,
+    )
+
+    parser.add_argument(
+        "--two",
+        dest="two_classes",
         action='store_true',
-        help="If you don't want to take into account the images with multiple different cells in it\033[0m"
+        help="If you only want two classes (no control patients)"
+    )
+
+    parser.add_argument(
+        "-n",
+        "--number_dataset",
+        default=1,
+        type=int,
+        help="If external validation, the number of different mapping between patient and fold. Default to 1.\033[0m",
+        required=False,
     )
 
     parser.set_defaults(extern_val=True)
+    parser.set_defaults(two_classes=False)
 
 
     return parser
 
 
-def create_csv(p: argparse.ArgumentParser):
-    CSV_PATH = p.parse_args().csv_path
-    fullDF = pd.read_csv(CSV_PATH)
+def main() -> None:
+    parser = parser_init()
 
-    st = p.parse_args().sub_type
-    no_multiple = p.parse_args().no_multiple
-    if st:
-        fullDF = fullDF[fullDF["type"].isin(st)].reset_index(drop=True)
-        st = '_'.join(st)+'_'
-    elif no_multiple:
-        fullDF = fullDF[fullDF["type"].str.find('+') == -1].reset_index(drop=True)
+    # Read CSV and create <Dataframe>
+    CSV_PATH = parser.parse_args().csv_path
+    fullDF = read_csv(CSV_PATH)
 
-    frac_sample = p.parse_args().frac_sample
-    extern_val = p.parse_args().extern_val
-    df_train_val_test(fullDF, st, no_multiple,frac_sample, extern_val)
+    extern_val = parser.parse_args().extern_val
+    two_classes = parser.parse_args().two_classes
+    k = parser.parse_args().number_fold
+    n = parser.parse_args().number_dataset
+    if n<0:
+        raise Exception(f"\033[38;5;160m[ERROR] 'n' cannot be negative but received : {n} \033[0m")
+    
+    if k<3:
+        raise Exception(f"\033[38;5;160m[ERROR] 'k' must be at least 3 for train/val/test repartition. Received: {k} \033[0m")
+    
+    if not os.path.exists(f"{os.getcwd()}/Dataset/"):
+        os.makedirs(f"{os.getcwd()}/Dataset/")
+    
+    for i in range(n):
+        full_pd_df = map_images_2_fold(full_pd_df=fullDF, extern_val=extern_val, k=k, two_classes=two_classes)
+
+        # Save CSV file
+        full_pd_df.to_csv(
+            f"{os.getcwd()}/Dataset/DS_Lymph_{"2" if two_classes else "3"}class_{"ext" if extern_val else "int"}_k{k}{f"_{i+1}" if n != 1 else ""}.csv",
+            index=False,
+            header=True,
+        )
 
 
 if __name__ == "__main__":
-    parser = parser_init()
-    create_csv(parser)
+    main()
--- lymphoma_dataset_class.py
+++ lymphoma_dataset_class.py
@@ -1,15 +1,17 @@
 """\033[38;5;224m    This is where the pytorch dataset class is initialized.
-    It's using pytorch dataset class and some transformation (Rescale, Crop, ToTensor & Normalize) thanks to torchvision.
-    the data are stored as variable 'image', 'categorie' and 'tabular with :
+    It's using pytorch dataset class and transformation (optional) thanks to torchvision.
+    It take as input a pandas dataframe with all the data from a csv dataset.
+    Optional, precomputed, used only if we use the precomuted version of an image instead (if the image has been saved as an numpy file in exit of the encoder)
+    the data are stored as variable 'image', 'classes' and 'tabular with :
             - image in the form of torch.tensor (3, 360, 360)
-            - categorie is either 'LCM' ('Lymphome à cellule du manteau') ou 'LZM' ('Lymphome de la zone marginale')
-            - tabular are all the metadata associated with each images\033[0m
+            - categorie is either 'LCM' ('Lymphome à cellule du manteau'), 'LZM' ('Lymphome de la zone marginale') or SGTEM (control patients)
+            - tabular are all the metadata associated with each images (here it is the 'patient', 'image_reference' and 'cell_type')\033[0m
 """
 
-from skimage import io, transform
+from numpy import load
+from skimage.io import imread
+from torch import is_tensor
 from torch.utils.data import Dataset
-import torch
-import numpy as np
 
 
 class LymphomaDataset(Dataset):
@@ -20,19 +22,19 @@ class LymphomaDataset(Dataset):
         self.precomputed = precomputed
         self.transform = transform
         self.classes = self.df["categorie"].unique().tolist()
-        self.tabular = self.df.drop(columns=["img_path", "categorie", "folder"])
+        self.tabular = self.df.drop(columns=["img_path", "categorie", "fold"])
         self.targets = self.df["categorie"].tolist()
 
     def __len__(self) -> int:
         return len(self.df)
 
     def __getitem__(self, index):
-        if torch.is_tensor(index):
+        if is_tensor(index):
             idx = idx.tolist()
         if self.precomputed:
-            image = np.load(self.df.iloc[index]["img_path"].replace(".jpg", ".npy"))
+            image = load(self.df.iloc[index]["img_path"].replace(".jpg", ".npy"))
         else:
-            image = io.imread(self.df.iloc[index]["img_path"]) / 255.0
+            image = imread(self.df.iloc[index]["img_path"]) / 255.0
             if self.transform:
                 image = self.transform(image)
         categorie = self.df.iloc[index]["categorie"]
